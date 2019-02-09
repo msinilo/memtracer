a6e6d75e8d916ea4ee224aa65634c36033f6f881
msinilo <maciejsinilo@o2.pl>
2019-02-08 22:59:03 -0500

    Added single header file (missed it before)

---
+++ memtracer/MemTracerSingleHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef MEMTRACER_ST_H
+#define MEMTRACER_ST_H
+
+#include "MemTracer.h"
+#include "RdeWrapper.h"
+#include "MemTracer.cpp"
+#include "DefaultFunctions.cpp"
+#include "DefaultSocketWrapperWin32.cpp"
+#include "RdeWrapper.cpp"
+
+#endif
